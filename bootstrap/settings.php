c298d0ebd17f0341411bc6e022a9abe4ce7bbaed
awei <awei01@gmail.com>
2016-02-02 21:29:04 -0500

    adding readme and some comments for code

--- bootstrap/settings.php
+++ bootstrap/settings.php
@@ -1,17 +1,21 @@
 <?php
+// disable stupid auto update
 define('DISABLE_WP_CRON', true);
 
 $server = 'http://' . $_SERVER['SERVER_NAME'];
+// shim the URL
 define('WP_SITEURL', $server . '/wp');
 define('WP_HOME', $server);
 
 $cwd = dirname(__FILE__);
 
+// set the content directory
 if (!defined('WP_CONTENT_DIR')) {
 	define( 'WP_CONTENT_DIR', realpath($cwd . '/../public/content' ));
 }
 define( 'WP_CONTENT_URL', $server . '/content' );
 
+// set the plugins directory
 if (!defined('WP_PLUGIN_DIR')) {
 	define( 'WP_PLUGIN_DIR', realpath($cwd . '/../public/content/plugins' ));
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Deployment
+* Use `composer install`
+* Ensure `public/wp` has symlink to `wp`
+* copy `.env.sample` to `.env` and set the configurations
