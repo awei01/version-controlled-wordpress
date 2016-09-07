5a56f957baf110ba6cdcac88c1bd0eab69f8b0db
Austin Wei <awei01@gmail.com>
2016-09-07 13:39:45 -0400

    adding debug, cron, revisions settings

--- bootstrap/configure-environment.php
+++ bootstrap/configure-environment.php
@@ -17,3 +17,7 @@
 define('DISALLOW_FILE_EDIT', getenv('DISALLOW_FILE_EDIT'));
 define('DISALLOW_FILE_MODS', getenv('DISALLOW_FILE_MODS'));
 
+define('WP_DEBUG', getenv('WP_DEBUG'));
+define('WP_POST_REVISIONS', getenv('WP_POST_REVISIONS'));
+define('DISABLE_WP_CRON', getenv('DISABLE_WP_CRON'));
+
