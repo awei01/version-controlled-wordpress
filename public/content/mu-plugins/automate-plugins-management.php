c20ff7e6ac93f0996fdc597b522f9b33745fb0d0
Austin Wei <awei01@gmail.com>
2016-09-07 11:32:42 -0400

    fixing init for installation

--- public/content/mu-plugins/automate-plugins-management.php
+++ public/content/mu-plugins/automate-plugins-management.php
@@ -4,12 +4,16 @@ function automate_plugins_management() {
   if (!getenv('AUTOMATE_PLUGINS_MANAGEMENT')) {
     return;
   }
+  if (!function_exists('get_plugins')) {
+    // we're probably installing WP so this function isn't available yet
+    return;
+  }
+
   // remove the plugins menu from admin nav
-	add_action('admin_menu', function() {
+  add_action('admin_menu', function() {
     remove_menu_page('plugins.php');
   });
 
-  // auto enable all the plugins
   $plugins = array_keys(get_plugins());
   $active = get_option('active_plugins');
   if (!is_array($active)) {
@@ -27,4 +31,5 @@ function automate_plugins_management() {
       do_action('activated_plugin', $plugin);
     }
   }
+
 }
