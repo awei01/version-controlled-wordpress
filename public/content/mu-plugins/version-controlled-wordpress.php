0cec296c7d58383c60db0c0188883a6b2b8bf551
Austin Wei <awei01@gmail.com>
2016-09-08 11:00:42 -0400

    moving muplugin to modules/hooks

---
+++ public/content/mu-plugins/version-controlled-wordpress.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ | -------------------
+ | Initialize
+ | -------------------
+
+ We'll put all our hooks here in one place so
+ we can limit the number of hooks that we're
+ introducing to the system
+
+ */
+
+// first, load developer hooks
+require ROOT_DIR . '/configure/hooks.php';
+
+// next, load our hooks
+require ROOT_DIR . '/modules/hooks/load.php';
+
