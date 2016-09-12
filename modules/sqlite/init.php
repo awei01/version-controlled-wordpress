ed1d72854f276f6dce968c60f7fe5e6c74eb8938
Austin Wei <awei01@gmail.com>
2016-09-12 12:15:00 -0400

    refactor sqlite module

--- modules/sqlite/init.php
+++ modules/sqlite/init.php
@@ -1,1 +1,17 @@
 <?php
+if (!getenv('SQLITE__enabled')) {
+  define('USE_MYSQL', true);
+  return;
+}
+
+$filename = getenv('SQLITE__filename') ?: 'database.sqlite';
+$path = getenv('SQLITE__storage_path') ?: ROOT_DIR . 'storage/database';
+if (!realpath($path)) {
+  throw new Exception('Invalid storage path for sqlite');
+}
+
+define('USE_MYSQL', false);
+define('DB_DIR', $path);
+define('DB_FILE', $filename);
+
+require WP_PLUGIN_DIR . '/sqlite-integration/db.php';
