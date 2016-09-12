ed2a92dd204afa7ad59f9d7a5da197a3c93cb201
Austin Wei <awei01@gmail.com>
2016-09-12 16:07:53 -0400

    suppressing tools menu item by default'

--- modules/hooks/suppress-admin-menu.php
+++ modules/hooks/suppress-admin-menu.php
@@ -2,15 +2,21 @@
 
 function suppress_admin_menu() {
   $config = getenv_array('SUPPRESS_ADMIN_MENU');
-
-  if (!empty($config['plugins'])) {
-    add_action('admin_menu', function() {
-      remove_menu_page('plugins.php');
-    });
+  if (!$config) {
+    return;
   }
-  if (!empty($config['themes_themes'])) {
-    add_action('admin_menu', function() {
-      remove_submenu_page('themes.php', 'themes.php');
+  foreach ($config as $key => $value) {
+    if (!$value) {
+      continue;
+    }
+    $parts = explode('_', $key);
+    $args = array_map(function($value) {
+      return $value . '.php';
+    }, $parts);
+    $method = sizeof($args) === 1 ? 'remove_menu_page' : 'remove_submenu_page';
+    add_action('admin_menu', function() use ($method, $args) {
+      call_user_func_array($method, $args);
     });
+
   }
 }
