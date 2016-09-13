1cb976c0da9aa225f44f759519811a83c414ac0e
Austin Wei <awei01@gmail.com>
2016-09-13 15:12:13 -0400

    refactor suppress_admin_menu hook to handle a string of menu items

--- modules/hooks/suppress-admin-menu.php
+++ modules/hooks/suppress-admin-menu.php
@@ -1,17 +1,19 @@
 <?php
 
 function suppress_admin_menu() {
-  $config = getenv_array('SUPPRESS_ADMIN_MENU');
-  if (!$config) {
+  $config = getenv('SUPPRESS_ADMIN_MENU') ?: '';
+  $items = explode('|', $config);
+  $items = array_filter($items);
+  if (!$items) {
     return;
   }
-  foreach ($config as $key => $value) {
-    if (!$value) {
-      continue;
-    }
-    $parts = explode('_', $key);
+  foreach ($items as $value) {
+    $parts = explode('>', $value);
     $args = array_map(function($value) {
-      return $value . '.php';
+      if (strpos($value, '.php') === false) {
+        $value .= '.php';
+      }
+      return $value;
     }, $parts);
     $method = sizeof($args) === 1 ? 'remove_menu_page' : 'remove_submenu_page';
     add_action('admin_menu', function() use ($method, $args) {
