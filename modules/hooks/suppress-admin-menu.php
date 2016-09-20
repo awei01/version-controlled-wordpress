086be3ffb076f86964689850ed672af091b3c13c
Austin Wei <awei01@gmail.com>
2016-09-20 09:53:46 -0400

    better logic for suppressing admin menu

--- modules/hooks/suppress-admin-menu.php
+++ modules/hooks/suppress-admin-menu.php
@@ -7,17 +7,17 @@ function suppress_admin_menu() {
   if (!$items) {
     return;
   }
-  foreach ($items as $value) {
-    $parts = explode('>', $value);
-    $args = array_map(function($value) {
-      if (strpos($value, '.php') === false) {
-        $value .= '.php';
-      }
-      return $value;
-    }, $parts);
-    $method = sizeof($args) === 1 ? 'remove_menu_page' : 'remove_submenu_page';
-    add_action('admin_menu', function() use ($method, $args) {
+  add_action('admin_menu', function() use ($items) {
+    foreach ($items as $value) {
+      $parts = explode('>', $value);
+      $args = array_map(function($value) {
+        if (strpos($value, '.php') === false) {
+          $value .= '.php';
+        }
+        return $value;
+      }, $parts);
+      $method = sizeof($args) === 1 ? 'remove_menu_page' : 'remove_submenu_page';
       call_user_func_array($method, $args);
-    });
-  }
+    }
+  });
 }
